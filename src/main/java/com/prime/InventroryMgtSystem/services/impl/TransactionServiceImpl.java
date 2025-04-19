ca21a77cca823e563e8055617c4e4b3c1b1c53ac
Abdulrahman Abdulrazaq <abdulrahmanabdulrazaq5@gmail.com>
2025-04-19 22:20:43 +0100

    Spring Boot Inventory Management system

--- src/main/java/com/prime/InventroryMgtSystem/services/impl/TransactionServiceImpl.java
+++ src/main/java/com/prime/InventroryMgtSystem/services/impl/TransactionServiceImpl.java
@@ -1,22 +1,36 @@
 package com.prime.InventroryMgtSystem.services.impl;
 
 import com.prime.InventroryMgtSystem.dtos.Response;
+import com.prime.InventroryMgtSystem.dtos.TransactionDTO;
 import com.prime.InventroryMgtSystem.dtos.TransactionRequest;
 import com.prime.InventroryMgtSystem.enums.TransactionStatus;
+import com.prime.InventroryMgtSystem.enums.TransactionType;
 import com.prime.InventroryMgtSystem.exceptions.NamevalueRequiredExecption;
 import com.prime.InventroryMgtSystem.exceptions.NotFoundExecption;
 import com.prime.InventroryMgtSystem.models.Product;
 import com.prime.InventroryMgtSystem.models.Supplier;
+import com.prime.InventroryMgtSystem.models.Transaction;
+import com.prime.InventroryMgtSystem.models.User;
 import com.prime.InventroryMgtSystem.reposit.CategoryRepository;
 import com.prime.InventroryMgtSystem.reposit.ProductRepository;
 import com.prime.InventroryMgtSystem.reposit.SupplierRepository;
 import com.prime.InventroryMgtSystem.reposit.TransactionRepository;
 import com.prime.InventroryMgtSystem.services.TransactionService;
 import com.prime.InventroryMgtSystem.services.UserService;
+import com.prime.InventroryMgtSystem.specification.TransactionFilter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -43,33 +57,169 @@ public class TransactionServiceImpl implements TransactionService {
         Supplier supplier = supplierRepository.findById(supplierId)
                 .orElseThrow(() -> new NotFoundExecption("Supplier Not Found"));
 
+        User user= userService.getCurrentLoggedInUser();
 
-        return null;
+        //update the stock quantity and re-save since we buy
+        product.setStockQuantity(product.getStockQuantity() + quantity);
+        productRepository.save(product);
+
+        Transaction transaction = Transaction.builder()
+                .transactionType(TransactionType.PURCHASE)
+                .status(TransactionStatus.COMPLETED)
+                .product(product)
+                .user(user)
+                .supplier(supplier)
+                .totalproducts(quantity)
+                .totalPrice(product.getPrice().multiply(BigDecimal.valueOf(quantity)))
+                .description(transactionRequest.getDescription())
+                .note(transactionRequest.getNote())
+                .build();
+
+        transactionRepository.save(transaction);
+        return Response.builder()
+                .status(200)
+                .message("Purchase Made successfully")
+                .build();
+
     }
 
     @Override
     public Response sell(TransactionRequest transactionRequest) {
-        return null;
+        Long productId = transactionRequest.getProductid();
+        Integer quantity = transactionRequest.getQuantity();
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new NotFoundExecption("Product Not Found"));
+
+        User user = userService.getCurrentLoggedInUser();
+
+
+        //update the stock quantity and re-save
+        product.setStockQuantity(product.getStockQuantity() - quantity);
+        productRepository.save(product);
+//create a transaction
+        Transaction transaction = Transaction.builder()
+                .transactionType(TransactionType.SALE)
+                .status(TransactionStatus.COMPLETED)
+                .product(product)
+                .user(user)
+                .totalproducts(quantity)
+                .totalPrice(product.getPrice().multiply(BigDecimal.valueOf(quantity)))
+                .description(transactionRequest.getDescription())
+                .note(transactionRequest.getNote())
+                .build();
+
+        transactionRepository.save(transaction);
+        return Response.builder()
+                .status(200)
+                .message("Product Sale successfully made")
+                .build();
+
+
     }
 
     @Override
     public Response returnToSupplier(TransactionRequest transactionRequest) {
-        return null;
+        Long productId = transactionRequest.getProductid();
+        Long supplierId = transactionRequest.getSupplierId();
+        Integer quantity = transactionRequest.getQuantity();
+
+        if (supplierId == null) throw new NamevalueRequiredExecption("Supplier Id is Required");
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new NotFoundExecption("Product Not Found"));
+
+        Supplier supplier = supplierRepository.findById(supplierId)
+                .orElseThrow(() -> new NotFoundExecption("Supplier Not Found"));
+
+        User user = userService.getCurrentLoggedInUser();
+
+        //update the stock quantity and re-save
+        product.setStockQuantity(product.getStockQuantity() - quantity);
+        productRepository.save(product);
+
+
+        //create a transaction
+        Transaction transaction = Transaction.builder()
+                .transactionType(TransactionType.RETURN)
+                .status(TransactionStatus.PROCESSING)
+                .product(product)
+                .user(user)
+                .totalproducts(quantity)
+                .totalPrice(BigDecimal.ZERO)
+                .description(transactionRequest.getDescription())
+                .note(transactionRequest.getNote())
+                .build();
+
+        transactionRepository.save(transaction);
+
+        return Response.builder()
+                .status(200)
+                .message("Product Returned in progress")
+                .build();
     }
 
     @Override
     public Response getAllTransactions(int page, int size, String filter) {
-        return null;
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
+
+        //user the Transaction specification
+        Specification<Transaction> spec = TransactionFilter.byFilter(filter);
+        Page<Transaction> transactionPage = transactionRepository.findAll(spec, pageable);
+
+        List<TransactionDTO> transactionDTOS = modelMapper.map(transactionPage.getContent(), new TypeToken<List<TransactionDTO>>() {
+        }.getType());
+
+        transactionDTOS.forEach(transactionDTO -> {
+            transactionDTO.setUser(null);
+            transactionDTO.setProduct(null);
+            transactionDTO.setSupplier(null);
+        });
+
+        return Response.builder()
+                .status(200)
+                .message("success")
+                .transactions(transactionDTOS)
+                .totalElement(transactionPage.getTotalElements())
+                .totalPages(transactionPage.getTotalPages())
+                .build();
+
     }
 
     @Override
     public Response getAllTransactionById(Long id) {
-        return null;
+        Transaction transaction = transactionRepository.findById(id)
+                .orElseThrow(() -> new NotFoundExecption("Transaction Not Found"));
+
+        TransactionDTO transactionDTO = modelMapper.map(transaction, TransactionDTO.class);
+
+        transactionDTO.getUser().setTransaction(null);
+
+        return Response.builder()
+                .status(200)
+                .message("success")
+                .transaction(transactionDTO)
+                .build();
     }
 
     @Override
     public Response getAllTransactionByMonthAndYear(int month, int year) {
-        return null;
+        List<Transaction> transactions = transactionRepository.findAll(TransactionFilter.byMonthAndYear(month, year));
+
+        List<TransactionDTO> transactionDTOS = modelMapper.map(transactions, new TypeToken<List<TransactionDTO>>() {
+        }.getType());
+
+        transactionDTOS.forEach(transactionDTO -> {
+            transactionDTO.setUser(null);
+            transactionDTO.setProduct(null);
+            transactionDTO.setSupplier(null);
+        });
+
+        return Response.builder()
+                .status(200)
+                .message("success")
+                .transactions(transactionDTOS)
+                .build();
     }
 
     @Override
